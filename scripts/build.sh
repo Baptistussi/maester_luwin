9d333d59a5932a52aed0dc3c99966b129f907388
Gabriel Lazarini Baptistussi <gabrielbap1@gmail.com>
2026-03-29 22:40:18 -0300

    fixing build.sh and cronjob.tf

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-mkdir -p dist
+rm -rf dist && mkdir -p dist
 pip install -t dist -r requirements.txt
-cd src && zip -r ../dist/cronjob.zip . -x "*.pyc" -x "__pycache__/*"
+cp -r src dist/
+cd dist && zip -r ../dist/cronjob.zip . -x "*.pyc" -x "__pycache__/*"
